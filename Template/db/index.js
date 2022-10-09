2783fddbc587fb75647590c54847fd5f7c774dd0
Corey Skidmore <skidmoreeco@gmail.com>
2022-10-09 16:06:33 -0400

    setting tables & inquirer prompts

--- Template/db/index.js
+++ Template/db/index.js
@@ -46,14 +46,70 @@ const questions = [
             message: 'Which deparetment does this role belong to?',
             name: 'department'
         }
+        ]).then((response) => {
+            const roles = new Role(response.addRole, response.role_id, response.salary, response.department)
+            roster.push(roles);
+            addPrompt();
+        })
+     }
 
+     function employeePrompt () {
+        inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What would you like to do?',
+            name: 'addEmployee'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees first name?',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees last name?',
+            name: 'lastName'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees role?',
+            name: 'employeeRole'
+        },
+        {
+            type: 'input',
+            message: 'Who is the employees manager?',
+            name: 'managerName'
+        }
+        ]).then((response) => {
+            const employee = new Employee(response.firstName, response.lastName, response.employeeRole, response.managerName)
+            roster.push(employee);
+            addPrompt();
+        })
+     }
 
+     function updateEmployee () {
+        inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What would you like to do?',
+            name: 'updateRole'
+        },
+        {
+            type: 'input',
+            message: 'Which employees role do you want to update?',
+            name: 'employeeList'
+        }
+        ]).then((response) => {
+            const update = new Update(response.updateRole, response.employeeList)
+            roster.push(update)
+            addPrompt();
+        })
+     }
+     function init() {
+        addPrompt()
+     };
 
-
-
-
-        ])
-    }
+    
 
 
 
